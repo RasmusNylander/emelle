a965e6c2cdf383d1806aa11732745165744a1bf2
Rasmus Nylander <43042651+RasmusNylander@users.noreply.github.com>
2022-04-19 16:35:57 +0200

    Regression: Fix validation error reported as test-error
    This makes the test error of the rlr be the error when the best rlr model is run in the test set, instead of the generalisation error on the training set of the best rlr model

--- linearRegression1.py
+++ linearRegression1.py
@@ -15,10 +15,13 @@ def regularised_linear_regression_model_weights(dataTdata: ndarray, dataTtruth: 
 	w_rlr = np.linalg.solve(dataTdata + λDiagonalMatrix, dataTtruth).squeeze()
 	return w_rlr
 
+
 def squared_error(truth: ndarray, predictions: ndarray) -> double:
 	return np.square(truth - predictions).mean(axis=0)
 
-def gen_error_given_λ(data: ndarray, truth: ndarray, folds: KFold, λs: ndarray) -> (ndarray, ndarray):
+
+def gen_error_and_weights_given_λ(data: ndarray, truth: ndarray, folds: KFold, λs: ndarray) -> (
+ndarray, ndarray, ndarray):
 	N, M = data.shape
 
 	# Add offset attribute
@@ -27,9 +30,9 @@ def gen_error_given_λ(data: ndarray, truth: ndarray, folds: KFold, λs: ndarray
 
 	# Error for each value of λ for each fold
 	error_train: ndarray = np.empty((len(λs), folds.n_splits))
-	error_test: ndarray = np.empty((len(λs), folds.n_splits))
+	error_test: ndarray = np.ones((len(λs), folds.n_splits))*np.inf
 
-	w_rlr: ndarray = np.empty(M)  # weights for regularized logistic regression
+	w_rlr: ndarray = np.empty(M, len(λs))  # weights for regularized logistic regression
 
 	for k, (train_index, test_index) in enumerate(folds.split(data, truth)):
 		# Extract training and test set for current CV fold
@@ -42,16 +45,25 @@ def gen_error_given_λ(data: ndarray, truth: ndarray, folds: KFold, λs: ndarray
 		dataTtruth: ndarray = data_train.T @ truth_train
 
 		for i in range(0, len(λs)):
-			w_rlr = regularised_linear_regression_model_weights(dataTdata, dataTtruth, λs[i])
-			error_train[i, k] = squared_error(truth_train, data_train @ w_rlr)
-			error_test[i, k] = squared_error(truth_test, data_test @ w_rlr)
+			w_rlr_temp = regularised_linear_regression_model_weights(dataTdata, dataTtruth, λs[i])
+			error_train[i, k] = squared_error(truth_train, data_train @ w_rlr_temp)
+			error_test[i, k] = squared_error(truth_test, data_test @ w_rlr_temp)
+			if error_test[i].min() > error_test[i, k]:
+				w_rlr[:, i] = w_rlr_temp
 
 	error_gen: ndarray = np.mean(error_test, axis=1)
 	error_μ_train: ndarray = np.mean(error_train, axis=1)
+	return error_gen, error_μ_train, w_rlr
+
+
+def gen_error_given_λ(data: ndarray, truth: ndarray, folds: KFold, λs: ndarray) -> (ndarray, ndarray):
+	error_gen, error_μ_train, _ = gen_error_and_weights_given_λ(data, truth, folds, λs)
 	return error_gen, error_μ_train
+
 
 def gen_error_given_λ_new_fold(data: ndarray, truth: ndarray, K: int, λs: ndarray) -> (ndarray, ndarray):
 	return gen_error_given_λ(data, truth, KFold(n_splits=K, shuffle=True), λs)
+
 
 if __name__ == '__main__':
 	(data, class_labels, UPDRS) = load_data("train_data.txt")
@@ -72,14 +84,15 @@ if __name__ == '__main__':
 	training_errors: ndarray = np.mean(train_errors_μ_λ, axis=0)
 
 	min_λ_index = np.argmin(generalisation_errors, axis=0)
-	rlr_model_optimal_λ = regularised_linear_regression_model_weights(projected_data.T @ projected_data, projected_data.T @ UPDRS, λs[min_λ_index])
+	rlr_model_optimal_λ = regularised_linear_regression_model_weights(projected_data.T @ projected_data,
+																	  projected_data.T @ UPDRS, λs[min_λ_index])
 	print(f"Optimal λ: {λs[min_λ_index]}\nModel weights: {rlr_model_optimal_λ}")
 
 	# Plot Generalisation error and mean train error as a function of λ
 	plt.figure(3, figsize=(23.4, 16.5))
 	plt.loglog(λs, generalisation_errors.T, 'r.-', λs, training_errors.T, 'b.-')
 	plt.axvline(x=λs[min_λ_index], color='k', linestyle='--')
-	#plt.xticks(plt.xticks()[0] + [λs[min_λ_index]])
+	# plt.xticks(plt.xticks()[0] + [λs[min_λ_index]])
 	plt.xlabel('Regularization factor (λ)')
 	plt.ylabel('Squared error (cross - validation)')
 	plt.legend(['Generalisation error', 'Train error'])
--- regression2.py
+++ regression2.py
@@ -7,7 +7,8 @@ from torch import tensor
 from tqdm import trange, tqdm
 
 from PCA import project_data_onto_pcs
-from linearRegression1 import gen_error_given_λ, regularised_linear_regression_model_weights, squared_error
+from linearRegression1 import gen_error_and_weights_given_λ, gen_error_given_λ, \
+	regularised_linear_regression_model_weights, squared_error
 from loadData import load_data
 from train_neural_net import train_neural_net
 
@@ -25,7 +26,8 @@ def gen_error_given_hidden_units(data: ndarray, truth: ndarray, folds: KFold, hi
 
 	data, truth = data.astype(np.float32), truth.astype(np.float32)
 
-	for k, (train_index, test_index) in tqdm(enumerate(folds.split(data, truth)), total=folds.n_splits, desc="ANN outer fold", unit="folds"):
+	for k, (train_index, test_index) in tqdm(enumerate(folds.split(data, truth)), total=folds.n_splits,
+											 desc="ANN outer fold", unit="folds"):
 		# Extract training and test set for current CV fold
 		data_train: tensor = torch.from_numpy(data[train_index])
 		truth_train: tensor = torch.from_numpy(truth[train_index])
@@ -54,6 +56,7 @@ def gen_error_given_hidden_units(data: ndarray, truth: ndarray, folds: KFold, hi
 	error_μ_train: ndarray = np.mean(error_train, axis=1)
 	return error_gen, error_μ_train
 
+
 def print_table(errors: ndarray, table_meta_info: ndarray):
 	table_string: str = "Outer fold\t\t\tANN\t\t\tLinear regression\t\t\tBaseline\n"
 	table_string += "i\t\thᵢ\tEᵢ Test\t\tλᵢ\tEᵢ Test\t\t\tEᵢ Test\n"
@@ -65,7 +68,7 @@ def print_table(errors: ndarray, table_meta_info: ndarray):
 if __name__ == '__main__':
 	(data, class_labels, UPDRS) = load_data("train_data.txt")
 	projected_data = project_data_onto_pcs(data, 0.9)
-	projected_data = np.concatenate((np.ones((projected_data.shape[0],1)),projected_data),1)
+	projected_data = np.concatenate((np.ones((projected_data.shape[0], 1)), projected_data), 1)
 
 	K = 5
 
@@ -81,21 +84,26 @@ if __name__ == '__main__':
 		UPDRS_train, UPDRS_test = UPDRS[train_index], UPDRS[test_index]
 		CV2 = KFold(n_splits=K, shuffle=True)
 
+		# Baseline
 		baseline_models: ndarray = np.empty((CV2.n_splits))
 		baseline_error_val: ndarray = np.empty((CV2.n_splits))
 		for j, (train_index2, test_index2) in enumerate(CV2.split(data_train)):
+			data_train2, data_test2 = data_train[train_index2], data_train[test_index2]
 			UPDRS_train2, UPDRS_test2 = UPDRS_train[train_index2], UPDRS_train[test_index2]
 			baseline_models[j] = np.mean(UPDRS_train2)
 			baseline_error_val[j] = squared_error(UPDRS_test2, np.ones((UPDRS_test2.shape[0])) * baseline_models[j])
+		error_val[i, 0] = squared_error(UPDRS_test,
+										np.ones((UPDRS_test.shape[0])) * baseline_models[baseline_error_val.argmin()])
 
+		# Regularised linear regression
 		(rlr_error_val, _) = gen_error_given_λ(data_train, UPDRS_train, CV2, λs)
-		(ann_error_val, _) = gen_error_given_hidden_units(data_train, UPDRS_train, CV2, hidden_units)
+		optimal_λ = λs[rlr_error_val.argmin()]
+		optimal_rlr = regularised_linear_regression_model_weights(data_train.Transpose() @ data_train,
+																	data_train.Transpose() @ UPDRS_train,
+																	optimal_λ)
+		error_val[i, 1], table_meta_info[i, 0] = squared_error(UPDRS_test, data_test @ optimal_rlr), optimal_λ
 
-		error_val[i, 0] = squared_error(UPDRS_test, np.ones((UPDRS_test.shape[0])) * baseline_models[baseline_error_val.argmin()])
-		error_val[i, 1], table_meta_info[i, 0] = rlr_error_val.min(), λs[rlr_error_val.argmin()]
+		# Artificial neural network
+		(ann_error_val, _) = gen_error_given_hidden_units(data_train, UPDRS_train, CV2, hidden_units)
 		error_val[i, 2], table_meta_info[i, 1] = ann_error_val.min(), hidden_units[ann_error_val.argmin()]
 	print_table(error_val, table_meta_info)
-
-
-
-
